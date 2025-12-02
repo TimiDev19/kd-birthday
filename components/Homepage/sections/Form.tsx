02ca8c3f47a159b47e7ac17b10082d58d05d6b42
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-12-02 19:20:23 +0100

    textarea fix

--- components/Homepage/sections/Form.tsx
+++ components/Homepage/sections/Form.tsx
@@ -6,7 +6,7 @@ const Form = () => {
         <div className=' w-[100vw] h-[100vh] flex items-center justify-center'>
             <form action="" className=' w-[90%] lg:w-[60%]'>
                 <input type="text" className=' p-2 border-2 border-[#010438] w-full rounded-xl mb-4 focus:outline-none' placeholder='Enter Your Name' />
-                <textarea name="" id="" cols="30" rows="10" className=' p-2 border-2 border-[#010438] w-full rounded-xl focus:outline-none mb-[10px]' placeholder='Enter Your Message'></textarea>
+                <textarea name="" id="" cols={30} rows={10} className=' p-2 border-2 border-[#010438] w-full rounded-xl focus:outline-none mb-[10px] resize-none' placeholder='Enter Your Message'></textarea>
                 <button className=' focus:outline-none text-sm lg:text-lg mx-2 p-2 border border-[#010438] rounded-2xl flex items-center justify-center font-extrabold bg-white/10 backdrop-blur-lg hover:bg-white/0 duration-700'>
                     <Image
                         src={Logo}
